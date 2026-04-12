5e370717daab950843db5dffd7d6c187d9b989ec
Rudy Snel <rudysnel@Mac-mini-van-Rudy.local>
2026-04-12 10:17:04 +0200

    Update Google site verification code

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
     "Uw gecertificeerde installateur in Kortenhoef. Betrouwbaar, snel en professioneel — altijd netjes achtergelaten. Dakwerk, zinkwerk, sanitair, water, CV-installatie en gas.",
   keywords: "installateur, dakwerk, zinkwerk, sanitair, CV-installatie, gas, Kortenhoef, Hilversum, Loosdrecht",
   verification: {
-    google: "F2X3tjIgBh0RCvpVG7gQu5XlbLGc-6asJhDVLkpk-5Q",
+    google: "z-dNvaC4bgMJbIrFHdioMbhvjkWl7xEjumcJ0KLBLhg",
   },
   alternates: {
     canonical: "https://www.smit-installatie-techniek.nl",
